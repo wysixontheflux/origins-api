2957749921b9084c6ab5c0213c0851c0884e00c9
Dodo Le Reporter <60071624+dodolereporter@users.noreply.github.com>
2023-09-07 01:08:35 +0200

    Update passport.js

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -9,14 +9,16 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 passport.use(
-
   new StrategyJwt(
     {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
     },
     ((jwtPayload, done) => prisma.user.findUnique({ where: { id: jwtPayload.id } })
-      .then((user) => done(null, user))
+      .then((user) => {
+        user.password = undefined;
+        done(null, user);
+      })
       .catch((err) => done(err)))
   )
 );
